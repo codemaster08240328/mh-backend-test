f6f98b69291e10a54ea6f3f3a43225761a263624
Code Master <codemaster@Codes-MacBook-Pro.local>
2021-02-12 16:39:54 -0800

    [feat]: add new route for export in admin replaced request with axios

--- admin/src/controller/index.js
+++ admin/src/controller/index.js
@@ -1,19 +1,39 @@
-const request = require("request")
+const axios = require("axios")
 const config = require("config")
 
+const {
+  investmentsServiceUrl,
+} = config
 
 const getInvestmentHandler = (req, res) => {
   const {id} = req.params
-  request.get(`${config.investmentsServiceUrl}/investments/${id}`, (e, r, investments) => {
-    if (e) {
+  axios.get(`${investmentsServiceUrl}/investments/${id}`)
+    .then(resp => {
+      res.send(resp.data)
+    })
+    .catch((e) => {
       console.error(e)
-      res.send(500)
-    } else {
-      res.send(investments)
-    }
-  })
+      res.sendStatus(500)
+    })
+}
+
+const exportInvestmentHandler = (req, res) => {
+  const {
+    investmentId,
+  } = req.params
+
+  res.send(investmentId)
+  // request.get(`${investmentsServiceUrl}/investments/${investmentId}`, (e, r, investments) => {
+  //   if (e) {
+  //     console.error(e)
+  //     res.send(500)
+  //   } else {
+  //     res.send(investments)
+  //   }
+  // })
 }
 
 module.exports = {
   getInvestmentHandler,
+  exportInvestmentHandler,
 }
--- admin/src/route.js
+++ admin/src/route.js
@@ -2,11 +2,13 @@ const controller = require("./controller")
 
 const {
   getInvestmentHandler,
+  exportInvestmentHandler,
 } = controller
 
 
 const apiRoutes = (app) => {
   app.get("/investments/:id", getInvestmentHandler)
+  app.post("/investments/export/:investmentId", exportInvestmentHandler)
 }
 
 module.exports = {
